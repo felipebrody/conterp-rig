95d93ad339c3f6b1b9f4c1b5c5faf4a41a2f33a2
felipebrody <133117469+felipebrody@users.noreply.github.com>
2023-05-16 15:28:49 -0300

    Database refatored

--- api/src/database/schema.sql
+++ api/src/database/schema.sql
@@ -17,32 +17,91 @@ CREATE TABLE users (
     FOREIGN KEY(rig_id) REFERENCES rigs(id)
 );
 
+CREATE TABLE oil_well (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    name VARCHAR
+)
+
 
 CREATE TABLE efficiencies (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     date DATE NOT NULL,
     rig_id UUID,
     user_id UUID,
-    gloss_detail_id UUID,
-    available_hours NUMERIC(10,2),
-    repair_detail_id UUID,
-    dtm_detail_id UUID,
-    fluid_ratio NUMERIC(10,2) NOT NULL,
-    equipment_ratio NUMERIC(10,2) NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
+    oil_well_id UUID,
     FOREIGN KEY(rig_id) REFERENCES rigs(id),
-    FOREIGN KEY (gloss_detail_id) REFERENCES gloss_details(id),
-    FOREIGN KEY (repair_detail_id) REFERENCES repair_details(id),
-    FOREIGN KEY (dtm_detail_id) REFERENCES dtm_details(id) 
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(oil_well_id) REFERENCES oil_well(id),
+
 );
 
 CREATE TABLE gloss_details (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    efficiency_id UUID,
+    FOREIGN KEY(efficiency_id) REFERENCES efficiencies(id)
+
+);
+
+CREATE TABLE gloss_periods (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     start_hour TIME NOT NULL,
     end_hour TIME NOT NULL,
-    description VARCHAR,
-    classification VARCHAR NOT NULL
+    classification VARCHAR,
+    description TEXT,
+    gloss_detail_id UUID,
+    FOREIGN KEY(gloss_detail_id) REFERENCES gloss_details(id)
+
 );
+
+
+CREATE TABLE repair_details (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    efficiency_id UUID,
+    FOREIGN KEY(efficiency_id) REFERENCES efficiencies(id)
+);
+
+CREATE TABLE repair_periods (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    start_hour TIME NOT NULL,
+    end_hour TIME NOT NULL,
+    classification VARCHAR,
+    description TEXT,
+    repair_detail_id UUID,
+    FOREIGN KEY(repair_detail_id) REFERENCES repair_details(id)
+);
+
+CREATE TABLE dtm_details (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    hours NUMERIC(10,2) NOT NULL,
+    distance VARCHAR NOT NULL
+    efficiency_id UUID,
+    FOREIGN KEY(efficiency_id) REFERENCES efficiencies(id)
+);
+
+CREATE TABLE fluid_ratio (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    ratio VARCHAR,
+    efficiency_id UUID,
+    FOREIGN KEY(efficiency_id) REFERENCES efficiencies(id)
+);
+
+CREATE TABLE equipment_ratio (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    ratio VARCHAR,
+    efficiency_id UUID,
+    FOREIGN KEY(efficiency_id) REFERENCES efficiencies(id)
+);
+
+
+
+
+
+
+
+
+
+
+==========================================================
 
 CREATE TABLE repair_details (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
