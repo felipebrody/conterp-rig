6646c6ecc89107112a4a01a9c38ba843e2eaa1c0
felipemarq <137407401+felipemarq@users.noreply.github.com>
2023-06-23 13:41:30 +0000

    Edited the database connection to render dashboard

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,20 +1,18 @@
-const { Client } = require('pg')
+const { Client } = require("pg");
 
 const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'root',
-    password: 'root',
-    database: 'conterp',
-    timezone: 'America/Sao_Paulo'
-})
+  host: "dpg-ciaphj98g3nden686mlg-a",
+  port: 5432,
+  user: "root",
+  password: "BkZou1LFOuWlkfQ7eCLZydMRuI8NOSxE",
+  database: "conterp",
+  timezone: "America/Sao_Paulo",
+});
 
 client.connect();
 
 exports.query = async (query, values) => {
-    const { rows } = await client.query(query, values)
+  const { rows } = await client.query(query, values);
 
-    return rows;
-}
-
-
+  return rows;
+};
